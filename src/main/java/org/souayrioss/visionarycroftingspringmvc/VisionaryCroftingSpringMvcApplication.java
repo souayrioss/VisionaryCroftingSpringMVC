89f38d222373ffa5734da819345a0cfaee75bbe0
Oussama Souayri <souayri.oussama@gmail.com>
2022-12-11 20:01:36 +0100

    project structure

---
+++ src/main/java/org/souayrioss/visionarycroftingspringmvc/VisionaryCroftingSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package org.souayrioss.visionarycroftingspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionaryCroftingSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionaryCroftingSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/souayrioss/visionarycroftingspringmvc/VisionaryCroftingSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.souayrioss.visionarycroftingspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionaryCroftingSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
